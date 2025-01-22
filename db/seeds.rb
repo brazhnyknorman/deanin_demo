2e0e13f6bc58816fb91afcf5bd54c6b86ac46b61
brazhnyknorman <brazhnyknorman@gmail.com>
2025-01-22 12:50:45 -0500

    Implement Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+User.create(email: '123@abc.net', password: 'password', password_confirmation: "password")
+
 10.times do |x|
-  Post.create(title: "Title #{x+1}", body: "lorem ipsum")
+  Post.create(title: "Title #{x+1}", body: "lorem ipsum", user_id: User.first.id)
 end
